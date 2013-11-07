df7a4e4cf7f815dd874f06969f2940950f2b53f8
Leon Timmermans <fawaka@gmail.com>
2013-11-07 14:20:30 +0100

    Switch to [MakeMaker::Awesome]

---
+++ inc/MyMakeMaker.pm
@@ -0,0 +1,36 @@
+package inc::MyMakeMaker;
+use Moose;
+
+extends 'Dist::Zilla::Plugin::MakeMaker::Awesome';
+
+use File::Spec;
+ 
+override _build_WriteMakefile_args => sub { +{
+	%{ super() },
+	EXE_FILES => [ map { File::Spec->catfile('script', $_) } qw{s2p psed} ]
+} };
+
+override _build_MakeFile_PL_template => sub {
+	my ($self) = @_;
+	my $template = super();
+ 
+	$template .= <<'TEMPLATE';
+package MY;
+
+sub postamble {
+	my $self = shift;
+
+	return $self->SUPER::postamble . <<'END';
+script/psed: script/s2p
+	$(CP) script/s2p script/psed
+END
+}
+TEMPLATE
+ 
+	return $template;
+};
+ 
+__PACKAGE__->meta->make_immutable;
+
+no Moose;
+
